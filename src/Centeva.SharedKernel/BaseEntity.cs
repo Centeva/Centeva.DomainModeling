44f39f01aa31d578c6c19267f2136cf5bffc6e16
Pete Deffendol <pete.deffendol@centeva.com>
2021-12-10 16:53:36 -0700

    Eliminate a compiler warning by setting private Id to default value

--- src/Centeva.SharedKernel/BaseEntity.cs
+++ src/Centeva.SharedKernel/BaseEntity.cs
@@ -2,7 +2,7 @@ namespace Centeva.SharedKernel;
 
 public abstract class BaseEntity<TId>
 {
-    public TId Id { get; private set; }
+    public TId Id { get; private set; } = default!;
 
     public List<BaseDomainEvent> Events { get; protected set; } = new();
 }
