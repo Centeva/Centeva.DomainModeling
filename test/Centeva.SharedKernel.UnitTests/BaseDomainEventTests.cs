72d2580287fbc0a8c7ec88a01625cbd72e3fa059
Pete Deffendol <pete.deffendol@centeva.com>
2021-12-14 11:04:14 -0700

    Add unit tests

---
+++ test/Centeva.SharedKernel.UnitTests/BaseDomainEventTests.cs
@@ -0,0 +1,20 @@
+using FluentAssertions;
+using Xunit;
+
+namespace Centeva.SharedKernel.UnitTests;
+
+public class BaseDomainEventTests
+{
+    public class TestEvent : BaseDomainEvent
+    {
+    }
+
+    [Fact]
+    public void Constructor_ShouldSetOccurredToCurrentTime()
+    {
+        var newEvent = new TestEvent();
+
+        // Not really another way to test this
+        newEvent.Occurred.Should().BeCloseTo(DateTimeOffset.UtcNow, TimeSpan.FromMilliseconds(100));
+    }
+}
---
+++ test/Centeva.SharedKernel.UnitTests/BaseEntityTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Centeva.SharedKernel.UnitTests;
+
+public class BaseEntityTests
+{
+    public class TestEntity : BaseEntity<int>
+    {
+
+    }
+
+    [Fact]
+    public void Constructor_ShouldInitializeEmptyEvents()
+    {
+        var entity = new TestEntity();
+
+        entity.Events.Should().BeEmpty();
+    }
+}
