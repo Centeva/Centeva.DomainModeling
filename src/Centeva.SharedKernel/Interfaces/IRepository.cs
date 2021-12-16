e083859ae8bd7e8d219bdb467e77fe256d8c8982
Pete Deffendol <pete.deffendol@centeva.com>
2021-12-16 10:08:45 -0700

    Add ability to defer persistence in repository methods
    This should let users batch up changes and then persist all at once
    at the end using SaveChangesAsync.

--- src/Centeva.SharedKernel/Interfaces/IRepository.cs
+++ src/Centeva.SharedKernel/Interfaces/IRepository.cs
@@ -6,36 +6,40 @@ public interface IRepository<TEntity> : IReadRepository<TEntity> where TEntity :
     /// Adds an entity in the database.
     /// </summary>
     /// <param name="entity">The entity to add.</param>
+    /// <param name="saveChanges">Indicates whether changes should be persisted immediately.  You should use <see cref="SaveChangesAsync"/> at some point if this is false.</param>
     /// <param name="cancellationToken">A CancellationToken to observe while waiting for the task to complete.</param>
     /// <returns>
     /// A task that represents the asynchronous operation.
     /// The task result contains the <typeparamref name="T" />.
     /// </returns>
-    Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default);
-    
+    Task<TEntity> AddAsync(TEntity entity, bool saveChanges = true, CancellationToken cancellationToken = default);
+
     /// <summary>
     /// Updates an entity in the database
     /// </summary>
     /// <param name="entity">The entity to update.</param>
+    /// <param name="saveChanges">Indicates whether changes should be persisted immediately.  You should use <see cref="SaveChangesAsync"/> at some point if this is false.</param>
     /// <param name="cancellationToken">A CancellationToken to observe while waiting for the task to complete.</param>
     /// <returns>A task that represents the asynchronous operation.</returns>
-    Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
-    
+    Task UpdateAsync(TEntity entity, bool saveChanges = true, CancellationToken cancellationToken = default);
+
     /// <summary>
     /// Removes an entity in the database
     /// </summary>
     /// <param name="entity">The entity to delete.</param>
+    /// <param name="saveChanges">Indicates whether changes should be persisted immediately.  You should use <see cref="SaveChangesAsync"/> at some point if this is false.</param>
     /// <param name="cancellationToken">A CancellationToken to observe while waiting for the task to complete.</param>
     /// <returns>A task that represents the asynchronous operation.</returns>
-    Task DeleteAsync(TEntity entity, CancellationToken cancellationToken = default);
-    
+    Task DeleteAsync(TEntity entity, bool saveChanges = true, CancellationToken cancellationToken = default);
+
     /// <summary>
     /// Removes the given entities in the database
     /// </summary>
     /// <param name="entities">The entities to remove.</param>
+    /// <param name="saveChanges">Indicates whether changes should be persisted immediately.  You should use <see cref="SaveChangesAsync"/> at some point if this is false.</param>
     /// <param name="cancellationToken">A CancellationToken to observe while waiting for the task to complete.</param>
     /// <returns>A task that represents the asynchronous operation.</returns>
-    Task DeleteRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
+    Task DeleteRangeAsync(IEnumerable<TEntity> entities, bool saveChanges = true, CancellationToken cancellationToken = default);
 
     /// <summary>
     /// Persists changes to the database.
