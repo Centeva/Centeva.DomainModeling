835a09304b29e0b79cafa9d05853f8ceceb25087
Pete Deffendol <pete.deffendol@centeva.com>
2022-04-27 17:00:57 -0600

    Fix typo

--- src/Centeva.SharedKernel/Interfaces/IAggregateRoot.cs
+++ src/Centeva.SharedKernel/Interfaces/IAggregateRoot.cs
@@ -3,7 +3,7 @@ namespace Centeva.SharedKernel.Interfaces;
 /// <summary>
 /// You can apply this interface to an entity to mark it as an aggregate
 /// root.  Repositories should only work with aggregate roots and child
-/// entities should be access and manipulated through the root.
+/// entities should be accessed and manipulated through the root.
 /// </summary>
 public interface IAggregateRoot
 {
