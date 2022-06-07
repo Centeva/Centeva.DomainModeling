063395426b79c583f015e01d74a1270edf87517f
Pete Deffendol <pete.deffendol@centeva.com>
2022-06-07 10:27:14 -0600

    Move obsolete `AsQueryable` method to its own interface
    This forces consumers of this library to explicitly implement their
    concrete repository as `IQueryableRepository` if they desire to use
    this method.
    
    +semver: breaking

--- src/Centeva.SharedKernel/Interfaces/IReadRepository.cs
+++ src/Centeva.SharedKernel/Interfaces/IReadRepository.cs
@@ -4,18 +4,5 @@ namespace Centeva.SharedKernel.Interfaces;
 
 public interface IReadRepository<TEntity> : IReadRepositoryBase<TEntity> where TEntity : class, IAggregateRoot
 {
-    /// <summary>
-    /// Returns an <see cref="IQueryable{T}"/> for retrieving entities of type <typeparamref name="TEntity"/>
-    /// </summary>
-    /// <remarks>
-    /// This exposes the internals of your storage mechanism or ORM, which could
-    /// result in unexpected behavior, such as SQL queries being materialized
-    /// late in the process.
-    /// </remarks>
-    /// <returns>
-    /// The repository's contents as an <see cref="IQueryable{T}"/>
-    /// </returns>
-    [Obsolete("Use Specifications instead as this exposes unmaterialized queries")]
-    IQueryable<TEntity> AsQueryable();
 }
 
--- src/Centeva.SharedKernel/Interfaces/IRepository.cs
+++ src/Centeva.SharedKernel/Interfaces/IRepository.cs
@@ -1,49 +1,7 @@
-﻿namespace Centeva.SharedKernel.Interfaces;
+﻿using Ardalis.Specification;
 
-public interface IRepository<TEntity> : IReadRepository<TEntity> where TEntity : class, IAggregateRoot
+namespace Centeva.SharedKernel.Interfaces;
+
+public interface IRepository<TEntity> : IRepositoryBase<TEntity> where TEntity : class, IAggregateRoot
 {
-    /// <summary>
-    /// Adds an entity in the database.
-    /// </summary>
-    /// <param name="entity">The entity to add.</param>
-    /// <param name="cancellationToken">A CancellationToken to observe while waiting for the task to complete.</param>
-    /// <returns>
-    /// A task that represents the asynchronous operation.
-    /// The task result contains the <typeparamref name="T" />.
-    /// </returns>
-    Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default);
-
-    /// <summary>
-    /// Updates an entity in the database
-    /// </summary>
-    /// <param name="entity">The entity to update.</param>
-    /// <param name="cancellationToken">A CancellationToken to observe while waiting for the task to complete.</param>
-    /// <returns>A task that represents the asynchronous operation.</returns>
-    Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
-
-    /// <summary>
-    /// Removes an entity in the database
-    /// </summary>
-    /// <param name="entity">The entity to delete.</param>
-    /// <param name="cancellationToken">A CancellationToken to observe while waiting for the task to complete.</param>
-    /// <returns>A task that represents the asynchronous operation.</returns>
-    Task DeleteAsync(TEntity entity, CancellationToken cancellationToken = default);
-
-    /// <summary>
-    /// Removes the given entities in the database
-    /// </summary>
-    /// <param name="entities">The entities to remove.</param>
-    /// <param name="cancellationToken">A CancellationToken to observe while waiting for the task to complete.</param>
-    /// <returns>A task that represents the asynchronous operation.</returns>
-    Task DeleteRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
-
-    /// <summary>
-    /// Persists changes to tracked entities to the database.
-    /// </summary>
-    /// <returns>
-    /// A task that represents the asynchronous operation.
-    /// The task result contains the number of entries saved.
-    /// </returns>
-    /// <param name="cancellationToken">A CancellationToken to observe while waiting for the task to complete.</param>
-    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
