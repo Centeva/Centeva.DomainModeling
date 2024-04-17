c057dfd0cfb8d8a6c8d8a14e508e1703ddb5d6c8
Pete Deffendol <pete.deffendol@centeva.com>
2024-04-17 16:37:21 -0600

    Update packages

---
+++ src/Centeva.DomainModeling.EFCore/PostPersistenceDomainEventInterceptor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace Centeva.DomainModeling.EFCore;
+
+internal class PostPersistenceDomainEventInterceptor : SaveChangesInterceptor
+{
+    private readonly IDomainEventDispatcher _dispatcher;
+
+    public PostPersistenceDomainEventInterceptor(IDomainEventDispatcher dispatcher)
+    {
+        _dispatcher = dispatcher;
+    }
+
+    public override async ValueTask<int> SavedChangesAsync(SaveChangesCompletedEventData eventData, int result, CancellationToken cancellationToken = default)
+    {
+        if (eventData.Context is null)
+        {
+            return await base.SavedChangesAsync(eventData, result, cancellationToken);
+        }
+
+        var entities = eventData.Context.GetEntitiesWithEvents();
+        await _dispatcher.DispatchAndClearEvents(entities, cancellationToken);
+
+        return await base.SavedChangesAsync(eventData, result, cancellationToken);
+    }
+}
